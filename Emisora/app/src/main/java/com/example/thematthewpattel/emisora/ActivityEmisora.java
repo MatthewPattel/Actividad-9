458fcca5fbf5de20bcba210ac2802d9df637e8a7
Matias <matiplastilineitor@hotmail.com>
2018-10-02 15:25:40 -0500

    Commit

---
+++ Emisora/app/src/main/java/com/example/thematthewpattel/emisora/ActivityEmisora.java
@@ -0,0 +1,25 @@
+package com.example.thematthewpattel.emisora;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class ActivityEmisora extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_emisora);
+    }
+
+    public void Emision(View view) {
+        Intent intent = new Intent();
+
+        intent.setAction("com.example.thematthewpattel");
+
+        intent.setFlags(Intent.FLAG_INCLUDE_STOPPED_PACKAGES);
+
+        sendBroadcast(intent);
+    }
+}
